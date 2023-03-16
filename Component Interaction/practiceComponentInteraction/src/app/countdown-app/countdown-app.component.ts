60e5d1a1b1722c410b3c201ea3f96c9be78f9b92
hoangnhan2003 <ngohoangnhan03@gmail.com>
2023-03-16 11:38:11 +0700

    countdown-app

--- Component Interaction/practiceComponentInteraction/src/app/countdown-app/countdown-app.component.ts
+++ Component Interaction/practiceComponentInteraction/src/app/countdown-app/countdown-app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-countdown-app',
@@ -6,10 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./countdown-app.component.css']
 })
 export class CountdownAppComponent implements OnInit {
+  stop = false;
+  time = 10;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
+  clickStop() {
+    this.stop = !this.stop;
+  }
+
+  countDown() {
+    const interval = setInterval(() => {
+      this.time--;
+      if (this.stop === true) {
+        clearInterval(interval);
+      }
+      if (this.time === 0) {
+        clearInterval(interval);
+      }
+    }, 1000);
+  }
+
+  reset() {
+    this.time = 10;
+    this.stop = false;
+  }
 }
